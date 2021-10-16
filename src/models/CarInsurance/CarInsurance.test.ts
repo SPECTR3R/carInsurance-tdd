b0f011adf9547ecfb5f0d445335812f8eb19897e
Armando del Río <adrio.coder@gmail.com>
2021-10-16 18:51:26 -0500

    Add reafactor for validation and megacoverage tests

--- src/models/CarInsurance/CarInsurance.test.ts
+++ src/models/CarInsurance/CarInsurance.test.ts
@@ -1,14 +1,26 @@
 import { CarInsurance, Product } from '../index'
 
 describe('CarInsurance class', () => {
-  it('Creates an instance of CarInsurance', () => {
-    const carInsurance = new CarInsurance([new Product('test', 1, 1)])
+  it('Creates an instance of CarInsurance as long as the name is included in the private property portfolio', () => {
+    const carInsurance = new CarInsurance([
+      new Product('Medium Coverage', 10, 20),
+      new Product('Full Coverage', 2, 0),
+      new Product('Low Coverage', 5, 7),
+      new Product('Mega Coverage', -1, 80),
+      new Product('Special Full Coverage', 5, 49),
+      new Product('Super Sale', 3, 6)
+    ])
     expect(carInsurance).toBeDefined()
+    try {
+      new CarInsurance([new Product('test', 1, 24)])
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error)
+    }
   })
 
   it('The price of a product is never negative.', () => {
     try {
-      new CarInsurance([new Product('test', 1, -5)])
+      new CarInsurance([new Product('Medium Coverage', 1, -5)])
     } catch (err) {
       expect(err).toBeInstanceOf(Error)
     }
@@ -16,18 +28,23 @@ describe('CarInsurance class', () => {
 
   it('The price of a product is never more than 50.', () => {
     try {
-      new CarInsurance([new Product('test', 1, 60)])
+      new CarInsurance([new Product('Medium Coverage', 1, 60)])
     } catch (err) {
       expect(err).toBeInstanceOf(Error)
     }
   })
 
   it('The price of "Mega Coverage" product should always be 80, sellIn counter should never be altered ', () => {
-    const carInsurance = new CarInsurance([new Product('Mega Coverage', 1, 80)])
-    expect(carInsurance).toBeDefined()
+    const carInsurance = new CarInsurance([new Product('Mega Coverage', 12, 80), new Product('Mega Coverage', 23, 80)])
+    carInsurance.updatePrice()
+
+    expect(carInsurance.products[0].sellIn).toEqual(12)
+    expect(carInsurance.products[1].sellIn).toEqual(23)
+    expect(carInsurance.products[0].price).toEqual(80)
+    expect(carInsurance.products[1].price).toEqual(80)
 
     try {
-      new CarInsurance([new Product('test', 1, 70)])
+      new CarInsurance([new Product('Medium Coverage', 1, 70)])
     } catch (err) {
       expect(err).toBeInstanceOf(Error)
     }
--- src/models/CarInsurance/CarInsurance.ts
+++ src/models/CarInsurance/CarInsurance.ts
@@ -3,13 +3,20 @@ type ProductsArr = Array<Product>
 export class CarInsurance {
   products: ProductsArr
 
-  constructor(productsArr: ProductsArr) {
-    productsArr.forEach(({ price, name }) => {
-      if (price < 0) throw new Error('The price of a product is never negative.')
+  private portfolio: string[] = ['Medium Coverage', 'Full Coverage', 'Low Coverage', 'Mega Coverage', 'Special Full Coverage', 'Super Sale']
+
+  private validateInputArr(productsArr: ProductsArr): void {
+    return productsArr.forEach(({ price, name }) => {
+      if (!this.portfolio.includes(name)) throw new Error('Invalid product name')
+      if (price < 0) throw new Error('The price of a product should never be negative.')
       if (price > 50 && name !== 'Mega Coverage')
         throw new Error('The price of a product is never more than 50, unless it is the "Mega Coverage" product.')
       if (price !== 80 && name === 'Mega Coverage') throw new Error('The price "Mega Coverage" product should be 80.')
     })
+  }
+
+  constructor(productsArr: ProductsArr) {
+    this.validateInputArr(productsArr)
     this.products = productsArr
   }
 
@@ -17,7 +24,7 @@ export class CarInsurance {
     return this.products.map(
       ({ name, sellIn, price }): Product => {
         switch (name) {
-          case 'full coverage':
+          case 'Mega Coverage':
             return { name, sellIn, price }
           default:
             throw new Error('Invalid product name')
